0952be64ed093db610262a16846cb72542144397
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-28 14:28:16 +0300

    finish the lab requerments and scraping for referances in wiki webpage

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -1,9 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
+URL = "https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher"
 
 
-def get_citations_count(URL):
+def get_references_needed_count(URL):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
+    references = soup.find_all(class_="reference")
+    return (f'\n The number of references is: {len(references)} \n')
+#################################################################################################
+def get_references_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    references = soup.find_all(class_="reference")
+    list = 0
+    for x in (references):
+        list += 1
+        print(f'{list}. references needed for report: \n {x.parent.text}')
+
+
+print(get_references_needed_count(URL))
+print(get_references_needed_report(URL))
